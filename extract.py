f4aa95316461f30395ab30649b5c539c54e7061a
nateejpg <nathan11.abreu@gmail.com>
2025-11-01 01:05:01 -0300

    Adicionando parte do download e do extract da ETL!

---
+++ extract.py
@@ -0,0 +1,25 @@
+import os
+import zipfile
+import pandas as pd
+
+# Folder
+RAW_FOLDER = "Downloads/Raw"
+EXTRACTED_FOLDER = "Downloads/Extracted"
+os.makedirs(EXTRACTED_FOLDER, exist_ok=True)
+
+# Extract
+for file in os.listdir(RAW_FOLDER):
+    if file.endswith(".zip"):
+        zip_path = os.path.join(RAW_FOLDER, file)
+        print(f"Extracting {file}...")
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(EXTRACTED_FOLDER)
+
+# Read
+for file in os.listdir(EXTRACTED_FOLDER):
+    print(f"\n--- {file} ---")
+    try:
+        df = pd.read_csv(os.path.join(EXTRACTED_FOLDER, file), sep = ';', nrows=5, encoding='latin1')
+        print(df.head())
+    except Exception as e:
+        print(f"Error reading {file}: {e}")
