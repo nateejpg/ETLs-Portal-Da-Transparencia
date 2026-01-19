380ee801e047cf86c4bbf36ea3eb628bb64b8fdf
nateejpg <nathan11.abreu@gmail.com>
2026-01-19 01:33:29 -0300

    Mudando config do airflow!

--- pipelines/orcamentos/load.py
+++ pipelines/orcamentos/load.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 
 # CREATE DW CONNECTION
 
-DB_HOST = "postgres-dw"
-DB_PORT = "5432"
-DB_NAME = "orcamentos_dw"
-DB_USER = "admin"
-DB_PASS = "password123"
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
